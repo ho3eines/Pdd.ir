1d3821dcde57afd11501b24414b3bbb58c9c75e6
Ho3ein <mtnprog@gmail.com>
2026-07-17 20:47:41 +0330

    fix: PddComponentBase - use OnAfterRender fallback to guarantee AppState subscription

--- Pdd.ir.Shared/Components/PddComponentBase.cs
+++ Pdd.ir.Shared/Components/PddComponentBase.cs
@@ -7,8 +7,22 @@ public abstract class PddComponentBase : ComponentBase, IDisposable
 {
     [Inject] protected IAppStateService AppState { get; set; } = default!;
 
+    private bool _subscribed;
+
     protected override void OnInitialized()
     {
+        Subscribe();
+    }
+
+    protected override void OnAfterRender(bool firstRender)
+    {
+        if (!_subscribed) Subscribe();
+    }
+
+    private void Subscribe()
+    {
+        if (_subscribed) return;
+        _subscribed = true;
         AppState.OnStateChanged += OnStateChanged;
     }
 
